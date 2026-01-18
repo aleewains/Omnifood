cce00db9a576734ef437e61f047f21f708c1168c
aleewains <aleewains@gmail.com>
2026-01-18 16:30:11 +0500

    final responsive

---
+++ script.js
@@ -0,0 +1,51 @@
+const yearEl = document.querySelector(".year");
+const year = new Date().getFullYear();
+yearEl.innerHTML = year;
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerNav = document.querySelector(".header");
+btnNav.addEventListener("click", function () {
+  headerNav.classList.toggle("nav-open");
+});
+
+///// smooth scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    if (link.classList.contains("main-nav-link"))
+      headerNav.classList.toggle("nav-open");
+  });
+});
+
+//// sticky navbar
+const sectionHeroEl = document.querySelector(".hero-section");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
